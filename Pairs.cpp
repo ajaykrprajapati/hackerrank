c2babd5be6b334a82d668f2371ed6dcc5a52f7e2
Pawan <pawan161096@gmail.com>
2015-08-07 03:09:24 +0530

    Update Pairs.cpp

--- Pairs.cpp
+++ Pairs.cpp
@@ -1,26 +1,22 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <cstdlib>
-#include <numeric>
-#include <sstream>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
- 
-int countp(int arr[], int n, int k)
-{
-    int count = 0;
+
+int main()
+    {
+    
+    int arr[1000000],n,k,count=0;
+
+    
+   cin>>n>>k;
+    for(int i=0;i<n;i++)
+        {
+        cin>>arr[i];
+        
+    }
+    
+
     sort(arr, arr+n); 
     int l = 0;
     int r = 0;
@@ -36,23 +32,8 @@ int countp(int arr[], int n, int k)
               l++;
          else
               r++;
-    }   
-    return count;
-}
+    } 
 
-int main()
-    {
-    
-    int arr[1000000],n,k,count=0;
-
-    
-   cin>>n>>k;
-    for(int i=0;i<n;i++)
-        {
-        cin>>arr[i];
-        
-    }
-    count=countp(arr,n,k);
 
     cout<<count;
     return 0;
